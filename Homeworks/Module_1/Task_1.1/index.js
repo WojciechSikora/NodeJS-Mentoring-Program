b0e1832283dee860ab6bacb765eedb97a22f460b
Wojciech Sikora <wojciech_sikora@epam.com>
2020-08-09 21:27:45 +0200

    Change from PR review

--- Homeworks/Module_1/Task_1.1/index.js
+++ Homeworks/Module_1/Task_1.1/index.js
@@ -1,8 +1,7 @@
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function (stringFromUser){
-    let reversedString;
-    reversedString = stringFromUser.split('').reverse().join('');
+    const reversedString = stringFromUser.split('').reverse().join('');
     process.stdout.write(reversedString + '\n\n');
 });
 
